70b5284e023c067294a55c218d0c75e86155c330
lhlavati <lukahlavati@gmail.com>
2019-08-29 14:15:05 +0200

    Kreiran HibernateUtil.java

---
+++ TaxiSluzbaNetBeans/src/main/java/hlavati/utility/HibernateUtil.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hlavati.utility;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Luka
+ */
+public class HibernateUtil {
+    
+    private static Session session;
+
+    protected HibernateUtil() {
+    }
+
+    public static Session getSession() {
+        if(session==null){
+            try {
+                session = new Configuration().configure().buildSessionFactory().openSession();
+                
+            } catch (Throwable e) {
+                //ovdeje će završiti ako imamo problema s hibernate.cfg.xml
+                throw new ExceptionInInitializerError(e);
+            }
+        }
+        
+        return session;
+    }
+    
+    
+}
