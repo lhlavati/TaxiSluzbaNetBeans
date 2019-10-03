971e7798998957a1aba8449dd8f737f9cb28326a
lhlavati <lukahlavati@gmail.com>
2019-10-03 02:36:12 +0200

    Dodane metode za pretvorbu Date u LocalDateTime i obrnuto

--- TaxiSluzbaNetBeans/src/main/java/hlavati/utility/Utility.java
+++ TaxiSluzbaNetBeans/src/main/java/hlavati/utility/Utility.java
@@ -5,18 +5,30 @@
  */
 package hlavati.utility;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 /**
  *
  * @author Luka
  */
 public class Utility {
-    
-    public static String getNazivAplikacije(){
+
+    public static String getNazivAplikacije() {
         return "Taxi služba APP";
     }
-    
-    public static boolean isDev(){
+
+    public static boolean isDev() {
         return true;
     }
-    
+
+    public static Date convertToDateViaSqlTimestamp(LocalDateTime dateToConvert) {
+        return java.sql.Timestamp.valueOf(dateToConvert);
+    }
+
+    public static LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 }
